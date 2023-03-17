b5bb6f03a127335fc2de1b3bba19bf39c684a329
Leandrosilva9180 <91633539+Leandrosilva9180@users.noreply.github.com>
2023-03-17 03:49:02 -0300

    d

--- news_classifier.py.py
+++ news_classifier.py.py
@@ -9,15 +9,12 @@ import mysql.connector
 import time
 import talib
 from datetime import datetime
-import os
-import json
-from bs4 import BeautifulSoup
 
 
 # Adicione suas chaves de API aqui
 api_key = "t73hewCNRNNgmgrDHIQdIMrnxVbwqBtDvM3hWXyss0TMoJ9qLzZ7GWAdbrNs0Cse"
 secret_key = "XHUv7B6hwiKXv71retq0w336StWs6Lj6hrzFwMgxkwNqFre0u0BaoBI1PIAq9auv"
-NEWS_API_KEY = "794cc19fbc9840dca1e989169783753d"
+
 # Configuração da conexão com a Binance
 exchange = ccxt.binance({
     'apiKey': api_key,
@@ -44,17 +41,26 @@ def create_database_connection():
 
 
 # Função para criar tabelas no banco de dados, a ser implementada posteriormente
-def create_tables_if_not_exists(connection):
+def create_tables_if_not_exists():
+    connection = create_database_connection()
     cursor = connection.cursor()
+
     cursor.execute("""
-        CREATE TABLE IF NOT EXISTS news_data (
+        CREATE TABLE IF NOT EXISTS orders (
             id INT AUTO_INCREMENT PRIMARY KEY,
-            news TEXT NOT NULL,
-            label ENUM('positive', 'negative', 'neutral') NOT NULL
+            order_id VARCHAR(255) NOT NULL,
+            symbol VARCHAR(10) NOT NULL,
+            order_type VARCHAR(10) NOT NULL,
+            side VARCHAR(10) NOT NULL,
+            amount FLOAT NOT NULL,
+            price FLOAT NOT NULL,
+            timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
         )
     """)
+
     connection.commit()
     cursor.close()
+    connection.close()
 
 
 #função para inserir informações sobre cada ordem no banco de dados:
@@ -96,108 +102,6 @@ def check_technical_conditions(rsi, macd_diff):
 
     return (rsi_buy_condition and macd_buy_condition, rsi_sell_condition and macd_sell_condition)
 
-class NewsClassifier:
-    def __init__(self, training_data=None):
-        self.vectorizer = TfidfVectorizer()
-        self.training_data = training_data if training_data else load_training_data()
-        if self.training_data:
-            self.classifier = self.train_classifier()
-        else:
-            self.classifier = None
-
-    def train_classifier(self):
-        try:
-            news_texts, news_labels = zip(*[row for row in self.training_data if row[1]])
-            X_train = self.vectorizer.fit_transform(news_texts)
-            classifier = MultinomialNB().fit(X_train, news_labels)
-            print("Classifier trained successfully")
-            return classifier
-        except Exception as e:
-            print(f"Error while training classifier: {e}")
-            return None
-    def analyze_news(self, sample_news):
-        if not self.classifier:
-            print("No training data available. The classifier will not function correctly.")
-            return "unknown"
-        else:
-            # Transforma o texto em uma matriz de recursos vetorizados
-            X_test = self.vectorizer.transform([sample_news])
-            # Usa o classificador treinado para prever o rótulo da notícia
-            predicted_label = self.classifier.predict(X_test)[0]
-            return predicted_label
-            
-            
-def load_training_data():
-    try:
-        with connection.cursor() as cursor:
-            cursor.execute("SELECT news, label FROM news_data")
-            return cursor.fetchall()
-    except Exception as e:
-        print(f"Error while loading training data: {e}")
-        return []
-
-
-def get_latest_bitcoin_news_from_newsapi():
-    url = f"https://newsapi.org/v2/everything?q=bitcoin&apiKey={NEWS_API_KEY}&pageSize=1&sortBy=publishedAt&language=en"
-    
-    response = requests.get(url)
-    
-    if response.status_code != 200:
-        print(f"Error fetching news from NewsAPI.org: {response.status_code}")
-        return None
-
-    news_data = response.json()
-    
-    if not news_data["articles"]:
-        print("No articles found.")
-        return None
-
-    latest_article = news_data["articles"][0]
-    latest_title = latest_article["title"]
-    
-    return latest_title
-
-def get_training_data_from_database():
-    connection = create_database_connection()
-    cursor = connection.cursor()
-    cursor.execute("SELECT news, news_label FROM news_data")
-    training_data = cursor.fetchall()
-    connection.close()
-    return training_data
-
-
-def insert_news_to_database(news, label):
-    try:
-        with connection.cursor() as cursor:
-            sql = "INSERT INTO news_data (news, label) VALUES (%s, %s)"
-            cursor.execute(sql, (news, label))
-            connection.commit()
-            print(f"News '{news}' added to the database with label '{label}'")
-    except Exception as e:
-        print(f"Error while inserting news to database: {e}")
-
-
-
-def get_latest_bitcoin_news():
-    url = 'https://www.coindesk.com/'
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    news_titles = [title.get_text(strip=True) for title in soup.find_all('a', class_='card-title')]
-    if news_titles:
-        return news_titles[0]
-    else:
-        return None
-
-def news_exists_in_database(news):
-    try:
-        with connection.cursor() as cursor:
-            sql = "SELECT COUNT(*) FROM news_data WHERE news = %s"
-            cursor.execute(sql, (news,))
-            result = cursor.fetchone()
-            return result[0] > 0
-    except Exception as e:
-        print(f"Error while checking if news exists in the database: {e}")
-        return False
 
 # Função para buscar notícias relacionadas ao Bitcoin usando uma API de notícias real
 def fetch_bitcoin_news():
@@ -228,13 +132,10 @@ def get_usdt_balance():
 
 # Função para executar a estratégia de negociação
 def execute_trading_strategy():
-    # Carregar os dados de treinamento do banco de dados e inicializar o classificador de notícias
-    training_data = get_training_data_from_database()
-    news_classifier = NewsClassifier(training_data)
-    # Restante do código de negociação
     news_list = fetch_bitcoin_news()
+
     for news in news_list:
-        news_label = analyze_news(news)
+        news_label = analyze_news(news_data, news)
         usdt_balance = get_usdt_balance()
 
         # Calculando indicadores técnicos
@@ -272,30 +173,13 @@ def execute_trading_strategy():
 
 
 def main():
-    connection = create_database_connection()
-    create_tables_if_not_exists(connection)
+    # Crie tabelas no banco de dados, se não existirem
+    create_tables_if_not_exists()
 
-    training_data = load_training_data()
-    if not training_data:
-        print("No training data available. The classifier will not function correctly.")
-    else:
-        news_classifier = NewsClassifier(training_data)
-
-        while True:
-            latest_news = get_latest_bitcoin_news()
-            print(f"Latest news: {latest_news}")
-            
-            if not news_exists_in_database(latest_news):
-                news_label = news_classifier.analyze_news(latest_news)
-                print(f"Label for the latest news: {news_label}")
-                insert_news_to_database(latest_news, news_label)
-            else:
-                print("News already exists in the database.")
-            
-            execute_trading_strategy()
-            time.sleep(60)
-
-    connection.close()
+    # Execute o robô de negociação
+    while True:
+        execute_trading_strategy()
+        time.sleep(60)  # Aguarde 60 segundos antes de verificar novamente
 
 if __name__ == "__main__":
     main()
